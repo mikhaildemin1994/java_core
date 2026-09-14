fde6edae2f1c7648746ef22ccf80447a67721c22
mikhaildemin1994 <velociraptor_m@bk.ru>
2026-09-14 09:12:21 +0600

    task2 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,10 @@ package org.example;
 
 public class Main {
     public static void main( String[] args ) {
-        CustomBuilder customBuilder = new CustomBuilder();
-        customBuilder.append("a");
-        customBuilder.append("b");
-        customBuilder.append("c");
-        System.out.println(customBuilder);
-        customBuilder.undo();
-        System.out.println(customBuilder);
+        // Выполняем проверку метода с последующим выводом в консоль
+        Converter converter = new Converter();
+        Integer[] integers = {1,2,3,3,4,5,5,5,6,6,7};
+        String[] strings = {"а", "а", "б", "в", "в", "в", "г", "д", "е", "f", "g", "g", "g"};
+        System.out.println(converter.convertArrayToMap(integers));
     }
 }
